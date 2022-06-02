137d0f71295868186a83570d6a00c0b04f0692e1
Mesesan Bogdan <mesebogdan@yahoo.com>
2022-06-02 15:55:13 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Mesesan Bogdan");
+        System.out.println("my first");
+
+        System.out.println("Hello");
+        System.out.println("Mesesan Bogdan Cornel");
+
+
+
+        int x = 4;
+        double y = 7;
+        double z = x + y;
+        System.out.println(z);
+        double b = x/y;
+        System.out.println(b);
+
+        double operatia1 = -5 + 8 * 6;
+        double operatia2 = (55+9) % 9;
+        float operatia3 = 20 + -3*5 / 8F;
+        float operatia4 = 5 + 15 / 3F * 2 - 8 % 3;
+
+        System.out.println(operatia1);
+        System.out.println(operatia2);
+        System.out.println(operatia3);
+        System.out.println(operatia4);
+
+        System.out.println("Lui Marcel ii plac operatiile matematice. \nLa examen a rezolvat prima problema iar rezultatul \na fost " + operatia1 + ", iar la problema 2 rezultatul a fost " + operatia2 + ". \nCerinta problemei 3 a fost sa adune suma dintre primele 2 rezultate, iar rezultatul a fost:" + (operatia1+operatia2));
+
+
+
+
+
+
     }
 }

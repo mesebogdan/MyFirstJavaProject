fd9d6dd6e323f35ef75edd32bd6d894ea2e4c931
Mesesan Bogdan <mesebogdan@yahoo.com>
2022-06-10 15:28:24 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        System.out.println("Mesesan Bogdan");
-        System.out.println("my first");
+        printmyname();
 
-        System.out.println("Hello");
-        System.out.println("Mesesan Bogdan Cornel");
-
-
-
-        int x = 4;
-        double y = 7;
-        double z = x + y;
-        System.out.println(z);
-        double b = x/y;
-        System.out.println(b);
-
-        double operatia1 = -5 + 8 * 6;
-        double operatia2 = (55+9) % 9;
-        float operatia3 = 20 + -3*5 / 8F;
-        float operatia4 = 5 + 15 / 3F * 2 - 8 % 3;
-
-        System.out.println(operatia1);
-        System.out.println(operatia2);
-        System.out.println(operatia3);
-        System.out.println(operatia4);
-
-        System.out.println("Lui Marcel ii plac operatiile matematice. \nLa examen a rezolvat prima problema iar rezultatul \na fost " + operatia1 + ", iar la problema 2 rezultatul a fost " + operatia2 + ". \nCerinta problemei 3 a fost sa adune suma dintre primele 2 rezultate, iar rezultatul a fost:" + (operatia1+operatia2));
+        System.out.println(operatia1(-5, 8, 6));
+        System.out.println(operatia2(55, 9, 9));
+        System.out.println(operatia3(20,-3, 5, 8));
+        System.out.println(operatia4(5, 15, 3, 2, 8, 3));
 
 
+        Calculator calc = new Calculator();
 
+        System.out.println(calc.medianumerelor(10, 8, 6));
+        System.out.println(calc.FahrenheittoCelsius(100));
+        System.out.println(calc.inchtocm(200));
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter distance in meters: ");
+        double distanta = scanner.nextDouble();
+        System.out.println("Please enter hours: ");
+        double ore = scanner.nextDouble();
+        System.out.println("Please enter minutes: ");
+        double minute = scanner.nextDouble();
+        System.out.println("Please enter seconds: ");
+        double secunde = scanner.nextDouble();
 
+        System.out.println("Your speed in m/s is: " + calc.speedinms(distanta, ore, minute, secunde) +
+                "\nYour speed in km/h is: " + calc.speedinkm(distanta, ore, minute, secunde) +
+                "\nYour speed in mph is: " + calc.speedinmile(distanta, ore, minute, secunde));
 
     }
+    public static void printmyname(){
+        System.out.println("Hello \nMesesan Bogdan");
+    }
+
+    public static int operatia1(int x, int y, int z) {
+        return x + y * z;
+    }
+    public static int operatia2(int x, int y, int z) {
+        return (x + y) % z;
+    }
+    public static double operatia3(double x, double y, double z, double p) {
+        return x + ( y * z / p);
+    }
+    public static double operatia4(double x, double y, double z, double p, double m, double n){
+        return x + ( y / z * p ) - ( m % n );
+
+    }
+
 }
---
+++ src/draft.java
@@ -0,0 +1,16 @@
+public class draft {
+  //  System.out.println(calc.speedinkm(50, 2, 40, 10));
+    //    System.out.println("viteza dumneavoastra in km/h este:" + calc.speedinkm(7000, 1, 0, 15) +
+      //          "\n viteza dumneavoastra in mile/h este: " + calc.speedinmile(7000, 1, 0, 15) +
+        //        "\n viteza dumneavoastra in m/s este: " + calc.speedinms(7000, 1, 0, 15));
+
+  //  double operatia1 = -5 + 8 * 6;
+  //  double operatia2 = (55 + 9) % 9;
+   // float operatia3 = 20 + (-3 * 5 / 8F);
+  //  float operatia4 = 5 + (15 / 3F * 2) - (8 % 3);
+
+  //      System.out.println(operatia1);
+    //    System.out.println(operatia2);
+    //    System.out.println(operatia3);
+     //   System.out.println(operatia4);
+}

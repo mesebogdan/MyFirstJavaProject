9dcb37fd5561b68ff6202d9017bfe6229dd93ff3
Mesesan Bogdan <mesebogdan@yahoo.com>
2022-06-23 23:52:52 +0300

    initial commit

---
+++ src/Main3.java
@@ -0,0 +1,33 @@
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        LogicalOperations op = new LogicalOperations();
+
+        int biggest= op.checkBiggerNumber(10, 56);
+        System.out.println("ex.3.Bigger number is: "+ biggest);
+
+        String textinput = "FastTrack2";
+        System.out.println("ex.4.Text to show: " + op.verifytext(textinput));
+
+        String text = "FastTrackIT";
+        int number = 2;
+        System.out.println("ex.5." + op.textandnumbertoshow(text, number));
+
+        int nr = 7;
+        System.out.println("ex.6." + op.snowshow(nr));
+
+        String greater= op.greaternumbers(99);
+        System.out.println("ex.7." + greater);
+
+        int numberno = 7;
+        op.switchnumber(numberno);
+
+        int numero = 99;
+        System.out.println("ex.9. Even number? : " + op.isNumberEven(numero));
+
+        int varsta = 19;
+        System.out.println("ex.10. Is he eligible to vote?: " + op.isEligibleToVote(varsta));
+
+    }
+}
